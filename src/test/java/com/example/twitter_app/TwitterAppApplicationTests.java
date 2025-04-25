681cd3e33784e8667afcbcc71b7fdb53879a3e6f
Kishan <kishana0427@gmail.com>
2025-04-25 05:49:08 +0530

    added code

---
+++ src/test/java/com/example/twitter_app/TwitterAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.twitter_app;
+
+import org.testng.annotations.Test;
+
+public class TwitterAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // Test logic for application context loading
+    }
+}
